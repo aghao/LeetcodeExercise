075291219365efe3dbaa05b03b9b58a6a698b7d2
aghao <yinhao158@hotmail.com>
2021-03-31 23:48:15 +0800

    Leetcode 修改提交：TwoSum

---
+++ src/Array/AddTwoNumbers.java
@@ -0,0 +1,27 @@
+package Array;/*
+ * @lc app=leetcode.cn id=2 lang=java
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+
+import Entity.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
+// @lc code=end
+
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import Array.TwoSum;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,4,6,8};
+        TwoSum t = new TwoSum();
+        t.twoSum(arr, 10);
+    }
+}
