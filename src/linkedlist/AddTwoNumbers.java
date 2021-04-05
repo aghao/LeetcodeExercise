41faa46eb2bab08f6de0eb4b7e910238f91cb4c7
aghao <yinhao158@hotmail.com>
2021-04-05 18:27:19 +0800

    1.增加 [2]两数相加 解法

---
+++ src/linkedlist/AddTwoNumbers.java
@@ -0,0 +1,61 @@
+package linkedlist;
+
+/**
+ * [2] 两数相加
+ * Definition for singly-linked list
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class AddTwoNumbers {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = new AddTwoNumbers().addTwoNumbers(l1, l2);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // 也可以使用dummyHead来指示链表头结点
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carry = sum / 10;
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        if (carry > 0)
+            tail.next = new ListNode(carry);
+        return head;
+    }
+}
