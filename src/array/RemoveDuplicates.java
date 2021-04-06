2cda0a3dd2412f84b43bb0ce79fa9c1d9eb0005f
aghao <yinhao158@hotmail.com>
2021-04-06 15:52:47 +0800

    1.增加 [26]删除有序数组中的重复项 解法

---
+++ src/array/RemoveDuplicates.java
@@ -0,0 +1,41 @@
+package array;
+
+/**
+ * [26] 删除有序数组中的重复项
+ * <p>
+ * Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+ * <p>
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,1,2]
+ * Output: 2, nums = [1,2]
+ * Explanation: Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the returned length.
+ */
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 3, 3, 3, 4, 4, 4, 4};
+        int result = new RemoveDuplicates().removeDuplicates(nums);
+        System.out.println(result);
+    }
+
+    public int removeDuplicates(int[] nums) {
+        // 采用长短指针i,n指示，相同元素则短指针不动
+        // 长指针每次增加1，若不同元素则短指针也加1
+        if (nums.length == 0)
+            return 0;
+        int n = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[n]) {
+                n++;
+                nums[n] = nums[i];
+            }
+        }
+        // 不用考虑超出数组长度的元素，输出用n限制
+        /*for (int j = 0; j < n; j++)
+            System.out.print(nums[j]);*/
+        return n + 1;
+    }
+}
